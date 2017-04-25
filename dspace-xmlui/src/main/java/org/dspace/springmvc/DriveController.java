4b7afd5b6e637fa970f179a067131af112e5e779
Ondrej Kosarko <ko_ok@centrum.cz>
2017-04-25 13:54:25 +0200

    Test with eperson name

--- dspace-xmlui/src/main/java/org/dspace/springmvc/DriveController.java
+++ dspace-xmlui/src/main/java/org/dspace/springmvc/DriveController.java
@@ -1,5 +1,9 @@
 package org.dspace.springmvc;
 
+import org.apache.log4j.Logger;
+import org.dspace.app.xmlui.utils.ContextUtil;
+import org.dspace.core.Context;
+import org.dspace.eperson.EPerson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.google.api.Google;
 import org.springframework.social.google.api.drive.DriveFilesPage;
@@ -8,11 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
+import java.sql.SQLException;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/drive")
 public class DriveController {
+
+	Logger log = Logger.getLogger(DriveController.class);
 
 	private final Google google;
 
@@ -29,7 +39,16 @@ public class DriveController {
 
 	@ResponseBody
 	@RequestMapping(value = "/test", method = GET)
-	public String getTestString(){
-		return "TEST";
+	public String getTestString(HttpServletRequest request){
+		try {
+			Context context = ContextUtil.obtainContext(request);
+			EPerson ePerson = context.getCurrentUser();
+			String name = ePerson != null ? ePerson.getName() : "Null eperson";
+			return String.format("TEST %s", name);
+
+		}catch (SQLException e){
+			log.error(e);
+			return "forward:/error";
+		}
 	}
 }
